c8a47feacd418541cc6062bc3ce407196599b90b
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-04 15:50:16 -0600

    yay git push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let addToy = false;
 
+const toyCollection = document.getElementById('toy-collection')
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -13,3 +15,45 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+// need fetch, after fetch req. add to DOM
+fetch('http://localhost:3000/toys')
+.then(response => response.json())
+.then(json => gotToys(json)) //callback funt. need def of fun. 
+// .then(gotToys) json gets passed as arg automatically
+
+function gotToys(toys) {
+  //debugger
+  toys.forEach(toy => {
+    createCard(toy)
+  });
+}
+
+function createCard(toy) {
+  // make a div with class card
+  const card = document.createElement('div')
+  card.classList.add('card')
+  
+  const h2 = document.createElement('h2')
+  h2.innerText = toy.name
+
+  const image = document.createElement('img')
+  image.classList.add('toy-avatar')
+  image.src = toy.image
+
+  //debugger
+  const para = document.createElement('p')
+  para.innerText = `${toy.likes} Likes`
+
+  const button = document.createElement('button')
+  button.classList.add('like-btn')
+  button.innerText = "Like <3"
+
+  toyCollection.appendChild(card)
+  card.appendChild(h2)
+  card.appendChild(image)
+  card.appendChild(para)
+  card.appendChild(button)
+
+}
